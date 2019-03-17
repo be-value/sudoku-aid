a30a9ae776bfc4134d0e4c8b4473a12b2edef87a
Jelle Be-Value <be-value@live.nl>
2019-03-17 12:16:46 +0100

    some basic styling applied, v1

--- src/components/CellUI/CellUI.tsx
+++ src/components/CellUI/CellUI.tsx
@@ -3,7 +3,7 @@ import { ICellUIState } from "./ICellUIState";
 import { ICellUIProps } from "./ICellUIProps";
 import { connect } from "react-redux";
 import { selectCell } from "../../utils/actions";
-
+import styles from "./CellUI.module.css";
 
 class CellUI extends React.Component<ICellUIProps, ICellUIState> {
   constructor(props: ICellUIProps) {
@@ -13,15 +13,36 @@ class CellUI extends React.Component<ICellUIProps, ICellUIState> {
 
   onClick = () => {
     this.props.selectCell(this.props.cell.name);
-  }
+  };
 
   public render(): JSX.Element {
     return (
-      <svg width={this.props.size} height={this.props.size} x={this.props.x} y={this.props.y} onClick={this.onClick}>
-        <rect x="0" y="0" width={this.props.size} height={this.props.size} style={{ stroke: "black", strokeWidth: 1, fill: "none" }}></rect>
-
-        // TODO: resize the fontsize to .9 ipv .5 AND show .value ipv .name
-        <text x={this.props.size*.25} y={this.props.size*.75} fontSize={this.props.size*.6}>{this.props.cell.name}</text>
+      <svg
+        width={this.props.size}
+        height={this.props.size}
+        x={this.props.x}
+        y={this.props.y}
+        onClick={this.onClick}
+        className={styles.svg}
+      >
+        <g className={styles.cell}>
+          <rect
+            x="0"
+            y="0"
+            width={this.props.size}
+            height={this.props.size}
+            className={styles.rect}
+          />
+          // TODO: resize the fontsize to .9 ipv .5 AND show .value ipv .name
+          <text
+            x={this.props.size * 0.25}
+            y={this.props.size * 0.75}
+            fontSize={this.props.size * 0.6}
+            className={styles.text}
+          >
+            {this.props.cell.name}
+          </text>
+        </g>
       </svg>
     );
   }
@@ -33,5 +54,7 @@ function mapDispatchToProps(dispatch: any): any {
   };
 }
 
-export default connect(null, mapDispatchToProps)(CellUI);
-
+export default connect(
+  null,
+  mapDispatchToProps
+)(CellUI);
