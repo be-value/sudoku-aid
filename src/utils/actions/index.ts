3739fda669ccc55169dbd576a1a4d3ae92d1d7a8
Jelle Be-Value <be-value@live.nl>
2019-03-30 10:50:53 +0100

    Implemented all steps for toggling cell options

--- src/utils/actions/index.ts
+++ src/utils/actions/index.ts
@@ -1,11 +1,17 @@
 import { IAction } from "./IAction";
-import { SELECT_CELL, CELL_INPUT } from "./constants";
+import { SELECT_CELL, CELL_INPUT, TOGGLE_VIEW_CELL_OPTIONS } from "./constants";
 
-// reduc action for selecting a new cell
+// redux action for selecting a new cell
 export function selectCell (payload: string | undefined): IAction {
   return { type: SELECT_CELL, payload };
 }
 
+// redux action for inputting/removing a value to/from a cell
 export function cellInput (payload: number | undefined ): IAction {
   return { type: CELL_INPUT, payload };
+}
+
+// redux action for toggling visibility of cell options
+export function toggleViewCellOptions (payload: boolean): IAction {
+  return { type: TOGGLE_VIEW_CELL_OPTIONS, payload };
 }
