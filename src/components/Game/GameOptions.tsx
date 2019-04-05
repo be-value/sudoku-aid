b8c2e61be463c1682fd1708300cb78825277e117
Jelle Be-Value <be-value@live.nl>
2019-04-05 21:15:11 +0200

    added label

--- src/components/Game/GameOptions.tsx
+++ src/components/Game/GameOptions.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Switch from "@material-ui/core/Switch";
 import Select from "@material-ui/core/Select";
+import FormControl from '@material-ui/core/FormControl';
 import { toggleViewCellOptions, toggleViewCellNames, selectSudokuType } from "../../utils/actions";
 import { connect } from "react-redux";
 import { IState } from "../../utils/store/IState";
 import { IGameOptionsProps } from "./IGameOptionsProps";
-import { FormGroup, FormControlLabel, MenuItem } from "@material-ui/core";
+import { FormGroup, FormControlLabel, MenuItem, InputLabel } from "@material-ui/core";
 import { SudokuType } from "./SudokuType";
 
 
@@ -53,14 +54,17 @@ class GameOptions extends Component<IGameOptionsProps> {
             label="cell options"
           />
         </FormGroup>
-        <Select
-          value={this.props.sudokuType as SudokuType}
-          onChange={this.chooseSudokuType}>
-          <MenuItem value={SudokuType._9x9}>9x9</MenuItem>
-          <MenuItem value={SudokuType._9x9Cross}>9x9 X</MenuItem>
-          <MenuItem value={SudokuType._6x6}>6x6</MenuItem>
-          <MenuItem value={SudokuType._6x6Cross}>6x6 X</MenuItem>
-        </Select>
+        <FormControl>
+          <InputLabel>Sudoku</InputLabel>
+          <Select
+            value={this.props.sudokuType as SudokuType}
+            onChange={this.chooseSudokuType}>
+            <MenuItem value={SudokuType._9x9}>9x9</MenuItem>
+            <MenuItem value={SudokuType._9x9Cross}>9x9 X</MenuItem>
+            <MenuItem value={SudokuType._6x6}>6x6</MenuItem>
+            <MenuItem value={SudokuType._6x6Cross}>6x6 X</MenuItem>
+          </Select>
+        </FormControl>
       </div>
     );
   }
